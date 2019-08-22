bbac5e0f5fce34f255dfc54b1f70062e828b8b18
MisterHuh <jaehuh86@yahoo.com>
2019-08-22 13:50:02 -0700

    Define a setView method in App that takes two parameters; name and params. setView replaces App's view state with the new name String and params Object.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -13,6 +13,16 @@ export default class App extends React.Component {
       }
     };
   }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
+  }
+
   render() {
     return (
       <div className="container border border-dark">
