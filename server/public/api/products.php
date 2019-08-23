97efad850b4e1318c5a80a601a48b9b94c381bf4
MisterHuh <jaehuh86@yahoo.com>
2019-08-22 18:22:43 -0700

    backend02 waiting approval

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,9 +1,15 @@
 <?php
 
 require_once("functions.php");
+require_once("db_connection.php");
 
 set_exception_handler("error_handler");
 $output = file_get_contents("./dummy-products-list.json");
+
+if ($conn) {
+  throw new Exception(mysqli_connect_error());
+};
+
 print($output);
 
 ?>
