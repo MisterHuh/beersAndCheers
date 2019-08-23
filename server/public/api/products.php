a0abece340d50daf4341f2d9393f53df63153eab
MisterHuh <jaehuh86@yahoo.com>
2019-08-23 16:50:11 -0700

    backend end 04. Waiting Approval

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -2,6 +2,7 @@
 
 require_once("functions.php");
 set_exception_handler("error_handler");
+startup();
 require_once("db_connection.php");
 
 $query = "SELECT * FROM `products` ORDER BY `price` DESC";
