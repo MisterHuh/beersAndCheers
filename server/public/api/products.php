e85963c384c41c01c148b1bab17e9c1ed08f59f0
MisterHuh <jaehuh86@yahoo.com>
2019-08-25 21:49:12 -0700

    WIP. stuck on first part

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -5,7 +5,15 @@ set_exception_handler("error_handler");
 startup();
 require_once("db_connection.php");
 
-$query = "SELECT * FROM `products` ORDER BY `price` DESC";
+if (empty($GET_["id"])) {
+  // $whereClause = " WHERE `id` = 1";
+  $whereClause = " WHERE IF(`id` > 9, id, [])"
+} else {
+  $whereClause = "";
+}
+
+// $query = "SELECT * FROM `products` ORDER BY `price` DESC";
+$query = "SELECT * FROM `products`" . $whereClause;
 $result = mysqli_query($conn, $query);
 
 
