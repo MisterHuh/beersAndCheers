00fd66be863b549e735f84e86060e0a4a4c83472
MisterHuh <jaehuh86@yahoo.com>
2019-08-26 10:42:04 -0700

    if conditional to check ID is valid

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -5,14 +5,16 @@ set_exception_handler("error_handler");
 startup();
 require_once("db_connection.php");
 
-if (empty($GET_["id"])) {
-  // $whereClause = " WHERE `id` = 1";
-  $whereClause = " WHERE IF(`id` > 9, id, [])"; // if "id" doesn't exist
-} else {
-  $whereClause = ""; // if "id" is NOT empty
-}
+$id = $_GET["id"];
 
-// $query = "SELECT * FROM `products` ORDER BY `price` DESC";
+if (empty($id)) {
+  $whereClause = "";
+  throw new Exception("error: no id inputted ");
+  exit;
+} else {
+  $whereClause = " WHERE `id` = {$id}";
+};
+
 $query = "SELECT * FROM `products`" . $whereClause;
 $result = mysqli_query($conn, $query);
 
