956f44d7c967412271eb90c3e4df5555e5506d9d
MisterHuh <jaehuh86@yahoo.com>
2019-08-27 19:34:24 -0700

    made the requested changes. waiting approval

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
-  return (
 
-    <CartSummaryItem setView={props.setView} cart={props.cart}/>
+  let indivItem = props.cart;
+
+  let price = 0;
+  function getPrice() {
+    for (let index = 0; index < indivItem.length; index++) {
+      price += indivItem[index]['price'];
+    }
+    return (price / 100).toFixed(2);
+  }
+
+  return (
+    <React.Fragment>
+      <div
+        onClick={() => { props.setView('catalog', {}); }}
+        className="w-25 text-primary">{`< Back to Catalog`}
+      </div>
+
+      <div className="mt-3">Item Total: ${getPrice()}</div>
+
+      {props.cart.map(cart => {
+        return (
+          <CartSummaryItem setView={props.setView} key={cart.id} indivItem={cart} />
+        );
+      })}
+    </React.Fragment>
   );
 }
 
