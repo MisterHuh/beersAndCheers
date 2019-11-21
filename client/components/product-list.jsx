ca2dd606c34f9b928319246d8c54867b4659e34f
MisterHuh <jaehuh86@yahoo.com>
2019-11-20 20:36:04 -0800

    added disclaimer

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -8,10 +8,13 @@ class ProductList extends React.Component {
     super(props);
     this.state = {
       products: [],
-      welcomeModal: !localStorage.ageVerified
+      welcomeModal: !localStorage.ageVerified,
+      noModal: false
     };
     this.getProducts = this.getProducts.bind(this);
-    this.toggle = this.toggle.bind(this);
+    this.ageVerified = this.ageVerified.bind(this);
+    // this.ageVerifiedNo = this.ageVerifiedNo.bind(this);
+    this.proceed = this.proceed.bind(this);
   }
 
   getProducts() {
@@ -22,9 +25,19 @@ class ProductList extends React.Component {
       });
   }
 
-  toggle() {
+  // ageVerified() {
+  //   localStorage.setItem('ageVerified', 'true');
+  //   this.setState({ welcomeModal: !this.state.welcomeModal });
+  // }
+
+  ageVerified() {
     this.setState({ welcomeModal: !this.state.welcomeModal });
+    this.setState({ noModal: !this.state.noModal });
+  }
+
+  proceed() {
     localStorage.setItem('ageVerified', 'true');
+    this.setState({ noModal: !this.state.noModal });
   }
 
   componentDidMount() {
@@ -41,13 +54,33 @@ class ProductList extends React.Component {
               <div className="mt-2">You must be at least 21 to enter</div>
               <div className=" my-3">Are you over 21?</div>
               <div className="d-flex flex-row">
-                <div onClick={() => this.toggle()}className="rounded text-white font-weight-bold m-auto px-2 py-1 bg-warning">Yes</div>
+                <div onClick={() => this.ageVerified()}className="rounded text-white font-weight-bold m-auto px-2 py-1 bg-warning">Yes</div>
                 <div className="px-5">or</div>
-                <div onClick={() => this.toggle()}className="rounded text-white font-weight-bold m-auto px-2 py-1 bg-danger" >No</div>
+                <div onClick={() => this.ageVerified()}className="rounded text-white font-weight-bold m-auto px-2 py-1 bg-danger" >No</div>
               </div>
             </div>
           </div>
         </Modal>
+
+        <Modal isOpen={this.state.noModal} backdrop="static" keyboard={false} className="">
+          <div className="rounded text-center">
+            <div className="welcomeModalHeader mt-3">Age Verification</div>
+            <div className="welcomeModal d-flex flex-column align-items-center">
+              <div className="mt-2">Please know that this is for</div>
+              <div className=" my-3">demonstration purposes only.</div>
+
+              <div className="d-flex flex-row">
+                <div onClick={() => this.proceed()} className="rounded text-white font-weight-bold m-auto px-2 py-1 bg-warning">Proceed</div>
+
+                {/* <div className="px-5">or</div> */}
+                {/* <div onClick={() => this.toggle()} className="rounded text-white font-weight-bold m-auto px-2 py-1 bg-danger" >No</div> */}
+
+              </div>
+
+            </div>
+          </div>
+        </Modal>
+
         <Jumbotron />
         <ProductListItem product={this.state.products} setView={this.props.setView}/>
       </React.Fragment>
