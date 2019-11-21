49fff81fc682a171fec6dbc973020bc937687608
MisterHuh <jaehuh86@yahoo.com>
2019-11-20 23:29:48 -0800

    added cursor to header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -18,18 +18,20 @@ export const Header = props => {
         <div className="navbarContainer d-flex flex-row align-items-center justify-content-center w-50">
           <div onClick={() => props.setView('catalog', '')} className="navbarContainer mr-auto">
             <img src={logo} className="mx-2 logo mb-2 d-inline" />
-            <div className="d-inline">Cheers & Beers</div>
+            <div className="title d-inline">Cheers & Beers</div>
           </div>
         </div>
 
         <div className="d-flex flex-row align-items-center justify-content-center w-50">
           <div className="ml-auto mr-3">
-            <div onClick={() => props.setView('about', '')} className="text-align-center">About</div>
+            <div onClick={() => props.setView('about', '')} className="about text-align-center">About</div>
           </div>
 
           <div className="mr-3">
-            <div onClick={() => props.setView('cart', '')} className="d-inline">Cart </div>
-            <Badge pill className="d-inline">{cartQuantity}</Badge></div>
+            <div onClick={() => props.setView('cart', '')} className="cart d-inline">Cart </div>
+            <Badge
+              pill onClick={() => props.setView('cart', '')}
+              className="cart d-inline">{cartQuantity}</Badge></div>
         </div>
 
       </div>
